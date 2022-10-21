bc68c8ea071a3bf4ad57daa6d04376e8f5d56ec8
CowboyAragorn <jacobman0313@gmail.com>
2022-10-21 19:16:19 -0400

    feat: add tests for reverse string and calculator functions

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ export function capitalize(str) {
   stringArr[0] = stringArr[0].toUpperCase();
   return stringArr.join("");
 }
+export function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+export const calculator = {
+  addition: (a, b) => {
+    return a + b;
+  },
+  subtraction: (a, b) => {
+    return a - b;
+  },
+  multiplication: (a, b) => {
+    return a * b;
+  },
+  division: (a, b) => {
+    return a / b;
+  },
+};
--- main.test.js
+++ main.test.js
@@ -1,8 +1,25 @@
-import { capitalize } from "./main";
+import { capitalize, reverseString, calculator } from "./main";
 
 test("capitalize", () => {
   expect(capitalize("alphabet")).toBe("Alphabet");
 });
 test("capitalize", () => {
   expect(capitalize("Alphabet")).toBe("Alphabet");
+});
+
+test("reverseString", () => {
+  expect(reverseString("garfield")).toBe("dleifrag");
+});
+
+test("addition", () => {
+  expect(calculator.addition(6, 4)).toBe(10);
+});
+test("subtraction", () => {
+  expect(calculator.subtraction(6, 2)).toBe(4);
+});
+test("multiplication", () => {
+  expect(calculator.multiplication(6, 2)).toBe(12);
+});
+test("division", () => {
+  expect(calculator.division(6, 2)).toBe(3);
 });
